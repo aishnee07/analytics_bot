e441370eecd5c94ec5daa9615d2b099a3795a3a1
aishnee07 <aishneeporwal@gmail.com>
2025-05-09 09:28:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'analytics-bot-1.onrender.com',
+      '.onrender.com' // This will allow all subdomains on onrender.com
+    ],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  }
 })
